4b5f69f83e7e607dd0f0dd275f929ba39afc9953
Adrian Vatchinsky <adrian@getuni.io>
2021-10-22 11:28:45 -0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'minimap',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
